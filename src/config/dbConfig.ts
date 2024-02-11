0a6b87c025972b63f7706fda2fad68d899d661b8
ashwinpanchal <ashwinpanchal.iitb@gmail.com>
2024-02-11 16:59:00 +0530

    Added dbConfig fix

---
+++ src/config/dbConfig.ts
@@ -0,0 +1,28 @@
+import { Schema, model } from "mongoose";
+
+interface UserInterface {
+  username: string;
+  password: string;
+}
+
+interface TodoInterface {
+  title: String;
+  description: String;
+  done: Boolean;
+  userId: String;
+}
+
+const userSchema = new Schema<UserInterface>({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+const todoSchema = new Schema<TodoInterface>({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  done: { type: Boolean, default: false },
+  userId: { type: String, required: true },
+});
+
+export const User = model<UserInterface>("User", userSchema);
+export const Todo = model<TodoInterface>("Todo", todoSchema);
